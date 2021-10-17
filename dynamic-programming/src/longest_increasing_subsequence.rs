b02062534d70dbd74ef0e95f8bab55e261e43ab2
liujun <liujun@rxthinking.com>
2021-10-17 12:16:41 +0800

    add solutions

--- dynamic-programming/src/longest_increasing_subsequence.rs
+++ dynamic-programming/src/longest_increasing_subsequence.rs
@@ -4,8 +4,17 @@ struct Solution;
 
 impl Solution {
     pub fn length_of_lis(nums: Vec<i32>) -> i32 {
-        // pass
-        return 4;
+        let mut dp = vec![1; nums.len()];
+        let mut ans = 1;
+        for i in 1..nums.len() {
+            for j in 0..i {
+                if nums[j] < nums[i] {
+                    dp[i] = dp[i].max(dp[j] + 1);
+                }
+            }
+            ans = ans.max(dp[i]);
+        }
+        ans
     }
 }
 
